fa30f36389bc1c79fd8a849aa0a014ff07167995
satwikteotia <satwikteotia45@gmail.com>
2025-08-20 10:48:19 +0530

    jsfunction expression

--- jswork/index.js
+++ jswork/index.js
@@ -3,6 +3,11 @@
 // const a=12;
 // a=30;
 // console.log(a);
-let a=12;
-a=30;
-console.log(a);
+// let a=12;
+// a=30;
+// console.log(a);
+//function expression
+const data=function(){
+    console.log("hi");
+}
+data();
